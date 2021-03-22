51e89efdfde9ae7a8d6ce3b6b2394b149e3f4efe
Nkl <nklnikil4@gmail.com>
2021-03-22 22:59:02 +0530

    Initialized project AMA

---
+++ src/test/java/com/cg/ama/AssetManagementAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cg.ama;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssetManagementAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
